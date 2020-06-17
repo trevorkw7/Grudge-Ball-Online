c704b286c2b93dc90f251c1d2c891b4bca0399fa
trevorkw7 <54345106+trevorkw7@users.noreply.github.com>
2020-06-17 03:12:57 -0700

    Added throwing physics and recycle bin

--- grudge-ball/src/Animation.js
+++ grudge-ball/src/Animation.js
@@ -1,25 +1,100 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
 
-const SCALE = 0.3;
-const OFFSET = 80;
+function Animation({ballShot, time}) {
 
-function drawBall(ctx, location){
-    ctx.fillStyle = 'white';
-    ctx.sha
-}
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [ball_x, setball_x] = useState(170);
+  const [ball_y, setball_y] = useState(350);
+  const [gameOver, setGameOver] = useState(false)
+  let gravity = 1.1;
+  let gravitySpeed = 0;
 
-function Animation(){
-    const canvasRef = React.useRef(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d")
+    context.scale(2,2)
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context;
+
+    drawBall();
+  }, [])
+
+  useEffect(()=>{
+    update();
+    drawBall();
+  })
+
+  useEffect(()=>{
+    if(gameOver){
+        contextRef.current.fillRect(0,0, window.innerWidth * 2,   
+            window.innerHeight * 2); 
+            contextRef.current.fillText("You Win!", 10, 40);
+    }
+  }, [gameOver])
+
+  const drawInitialScene = () => {
+
+    contextRef.current.beginPath()
+    contextRef.current.arc(100, 300, 50, 0, 2 * Math.PI);
+    contextRef.current.stroke();
+    contextRef.current.moveTo(100, 350);
+    contextRef.current.lineTo(100, 500);
+    contextRef.current.stroke();
+    contextRef.current.lineTo(150, 600);
+    contextRef.current.stroke();
+    contextRef.current.moveTo(100, 500);
+    contextRef.current.lineTo(50, 600);
+    contextRef.current.stroke();
+    contextRef.current.moveTo(100, 450);
+    contextRef.current.lineTo(150, 350);
+    contextRef.current.stroke();
+
+    var imageObj  = new Image();
+    imageObj.src = "/recycle.png"
+    contextRef.current.drawImage(imageObj,800,500)
     
-    return(
-        <canvas
-            width={window.innerWidth}
-            height={window.innerHeight}
-            onClick={e => {
-                alert(e.clientX)
-            }}
-        />
-    )
+
+
+  }
+
+  const drawBall = () => {
+    contextRef.current.fillStyle = 'white'
+
+    contextRef.current.fillRect(0,0, window.innerWidth * 2,   
+        window.innerHeight * 2); 
+    drawInitialScene()
+    
+    contextRef.current.beginPath()
+    contextRef.current.arc(ball_x, ball_y, 20, 0, 2 * Math.PI);
+    contextRef.current.stroke();
+  }
+
+  function update(){
+      if(!ballShot){
+          return;
+      }
+        gravitySpeed += gravity;
+        let normTime = Math.min(time / 1000, 5);
+        let new_power = (normTime/5);
+        let new_x  = ball_x + new_power*2
+        let new_y = (ball_y - new_power) + Math.pow(1.01, gravitySpeed)
+        setball_x(new_x)
+        setball_y(Math.max(Math.min((new_y), window.innerHeight), 0))
+  }
+
+  return (
+    <canvas
+      ref={canvasRef}
+    />
+  );
 }
 
 export default Animation;
--- grudge-ball/src/App.js
+++ grudge-ball/src/App.js
@@ -14,28 +14,26 @@ function App() {
   let startTime = null;
   let endTime = null
   const [timeElapsed, setTimeElasped] = React.useState(0);
-  const [powerPercent, setPowerPercent] = React.useState(0);
   const [ballShot, setBallShot] = React.useState(false);
+  const [percent, setPercent] = React.useState(0);
 
-  let angle = 45;
+
   let limiter = 0;
-  let xInitial = 1020;
-  let yInitial = 390;
 
   function downHandler({ key }) {
     if (key === ' ' || key === 'Spacebar') {
       if (limiter === 0) {
         setTimeElasped(0)
-        setPowerPercent(0);
+        setPercent(0);
         startTime = Date.now()
         limiter++;
       }
-      else {
+      else { 
         //always null even after startTime is rendered
         setTimeElasped(Date.now() - startTime)
         let normTime = Math.min(timeElapsed / 1000, 5);
         let percentage = (normTime / 5) * 100;
-        setPowerPercent()
+        setPercent(percentage)
       }
     }
   }
@@ -44,20 +42,13 @@ function App() {
     if (key === ' ' || key === 'Spacebar') {
       let stopTime = Date.now()
       endTime = stopTime
-      calculateFinalPosition()
       limiter = 0;
+      setPercent(getPercentage())
+      setBallShot(true);
     }
   }
 
-  function calculatePosition() {
-    //given initial x, initial y, initial velocity, 
-    const initialVelocity = timeElapsed * 20;
-    let vX = initialVelocity * Math.cos(Math.PI * angle)
-    let vY = initialVelocity * Math.sin(Math.PI * angle)
-    this.x += vX;
-    this.y -= (vY - (9.81 * time));
 
-  }
 
 
   useEffect(() => {
@@ -73,27 +64,23 @@ function App() {
   function getPercentage() {
     let normTime = Math.min(timeElapsed / 1000, 5);
     let percentage = (normTime / 5) * 100;
-    console.log(percentage)
     return percentage
   }
 
-  function renderBall() {
-    return <img src='./ball.png' style={{ height: "40px", width: "40px", position: "absolute", top: "390px", right: "1020px" }} />
-  }
 
   return (
     <div className="App">
       <br />
       <br />
       <Grid container>
-        <Grid item xs>
+        <Grid item xs={3}>
           <ChargeBar variant="determinate" progress={getPercentage()} />
           <Typography>
             Power
           </Typography>
         </Grid>
-        <Grid item xs>
-          <Animation/>
+        <Grid item xs={9}>
+          <Animation ballShot={ballShot} time={timeElapsed}/>
         </Grid>
       </Grid>
     </div>
